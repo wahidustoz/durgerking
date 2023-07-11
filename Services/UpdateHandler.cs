c27ad30105c6554122e6e6a664e4bc38d98ab472
inogom <inogomxamidullayev@gmail.com>
2023-07-11 22:03:31 +0500

    Upset User When Update Arrives commited with changes

--- Services/UpdateHandler.cs
+++ Services/UpdateHandler.cs
@@ -10,7 +10,6 @@ public class UpdateHandler : IUpdateHandler
 {
     private readonly ILogger<UpdateHandler> logger;
     private readonly IServiceScopeFactory serviceScopeFactory;
-
     public UpdateHandler(
         ILogger<UpdateHandler> logger,
         IServiceScopeFactory serviceScopeFactory)
@@ -18,13 +17,11 @@ public class UpdateHandler : IUpdateHandler
         this.logger = logger;
         this.serviceScopeFactory = serviceScopeFactory;
     }
-
     public Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
     {
         logger.LogError(exception, "Polling error happened.");
         return Task.CompletedTask;
     }
-
     public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
         logger.LogInformation(
@@ -34,17 +31,13 @@ public class UpdateHandler : IUpdateHandler
 
         await UpsetUserAsync(update, cancellationToken);
     }
-
     private async Task UpsetUserAsync(Update update, CancellationToken cancellationToken)
     {
         var telegramUser = GetUserFromUpdate(update);
-
         using (var scope = serviceScopeFactory.CreateScope())
         {
             var dbContext = scope.ServiceProvider.GetRequiredService<IAppDbContext>();
-
             var user = await dbContext.Users.FirstOrDefaultAsync(u => u.Id == telegramUser.Id);
-
             if(user is null)
             {
                 dbContext.Users.Add(new DurgerKing.Entity.User
@@ -63,14 +56,11 @@ public class UpdateHandler : IUpdateHandler
                 user.Fullname = $"{telegramUser.FirstName} {telegramUser.LastName}";
                 user.Username = telegramUser.Username;
                 user.Language = telegramUser.LanguageCode;
-                user.CreatedAt = DateTime.UtcNow;
-                user.ModifiedAt = DateTime.UtcNow;
                 logger.LogInformation("New user with ID {id} updated.", telegramUser.Id);
             }
             await dbContext.SaveChangesAsync(cancellationToken);
         }
     }
-
     private User GetUserFromUpdate(Update update)
         => update.Type switch
         {
@@ -78,6 +68,6 @@ public class UpdateHandler : IUpdateHandler
             Telegram.Bot.Types.Enums.UpdateType.EditedMessage => update.EditedMessage.From,
             Telegram.Bot.Types.Enums.UpdateType.CallbackQuery => update.CallbackQuery.From,
             Telegram.Bot.Types.Enums.UpdateType.InlineQuery => update.InlineQuery.From,
-            _ => throw new Exception("We dont supportas update type {update.Type} yet") 
+            _ => throw new Exception("We don't support update type {update.Type} yet") 
         };   
 }
