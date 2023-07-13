e75808f8a64f537632e05d8f4429ab39fa01c081
tparizoda <parizodatogaeva@gmail.com>
2023-07-13 03:48:58 -0700

    first version

--- Services/UpdateHandler.cs
+++ Services/UpdateHandler.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
 using Telegram.Bot.Types.Enums;
 
 namespace DurgerKing.Services;
@@ -29,7 +30,7 @@ public partial class UpdateHandler : IUpdateHandler
     public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
         logger.LogInformation(
-            "Update {updateType} received from {userId}.",
+            "Update {updateType} received  from {userId}.",
             update.Type,
             update.Message?.From?.Id);
 
@@ -76,8 +77,9 @@ public partial class UpdateHandler : IUpdateHandler
                 user.Fullname = $"{telegramUser.FirstName} {telegramUser.LastName}";
                 user.Username = telegramUser.Username;
                 user.Language = telegramUser.LanguageCode;
+                user.CreatedAt = DateTime.UtcNow;
                 user.ModifiedAt = DateTime.UtcNow;
-                logger.LogInformation("User with Id {id} updated",telegramUser.Id);
+                logger.LogInformation("user with ID {id} updated.", telegramUser.Id);
             }
             await dbContext.SaveChangesAsync(cancellationToken);
         }
@@ -90,6 +92,29 @@ public partial class UpdateHandler : IUpdateHandler
             Telegram.Bot.Types.Enums.UpdateType.EditedMessage => update.EditedMessage.From,
             Telegram.Bot.Types.Enums.UpdateType.CallbackQuery => update.CallbackQuery.From,
             Telegram.Bot.Types.Enums.UpdateType.InlineQuery => update.InlineQuery.From,
-            _ => throw new Exception("We don't support update type {update.Type} yet") 
+            _ => throw new Exception("We dont supportas update type {update.Type} yet") 
         };   
+
+
+    private async Task SelectSettingsAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        var settingkeyboard = new InlineKeyboardMarkup(
+            new InlineKeyboardButton[][]
+            {
+                new InlineKeyboardButton[]
+                {
+                    InlineKeyboardButton.WithCallbackData(text: "Language 🎏", "language"),
+                    InlineKeyboardButton.WithCallbackData(text: "Locations 📌","locations"),
+                    InlineKeyboardButton.WithCallbackData(text: "Phone ☎️", "phone"),
+                }
+            }
+        );
+
+        await botClient.SendTextMessageAsync(
+            chatId: update.Message.Chat.Id,
+            text: "Choose an option to update your settings:",
+            replyMarkup: settingkeyboard,
+            cancellationToken: cancellationToken);
+    }
+    
 }
