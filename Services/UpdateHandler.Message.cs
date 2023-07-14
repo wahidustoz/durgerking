c600cc53ee671e097971fef7b62e98d965a4272d
xojimurodqosharov <xojimurodqosharov@gmail.com>
2023-07-14 21:07:25 +0500

    Send-menu-button

--- Services/UpdateHandler.Message.cs
+++ Services/UpdateHandler.Message.cs
@@ -12,21 +12,27 @@ public partial class UpdateHandler
             ?? message.From.FirstName;
         logger.LogInformation("Reieved message from {username}", username);
 
-        if(message.Text == "/start" || message.Text == "/help")
+        if (message.Text == "/start" || message.Text == "/help")
             await SendGreetingMessageAsycn(botClient, message, cancellationToken);
-        else if(message.Text == "/settings")
+        else if (message.Text == "/settings")
             await SelectSettingsAsync(botClient, message, cancellationToken);
     }
 
     private async Task SendGreetingMessageAsycn(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
     {
-        var username = message.From?.Username
-            ?? message.From.FirstName;
+        var username = message.From?.Username ?? message.From.FirstName;
         var greeting = $"Greetings, hungry {username}! 🌟🍔 Durgerking is here to make your day scrumptiously delightful. Join us for a burger adventure filled with sizzling flavors, crunchy goodness, and pure happiness. Get ready to take a big bite and experience burger heaven! 🍟😍";
-        
+
+        var replyKeyboardMarkup = new ReplyKeyboardMarkup(new KeyboardButton[][]
+            {
+                new KeyboardButton[] { "Settings ⚙️", "Menu 🍔" },
+                new KeyboardButton[] { "Orders 📝" },
+            }) { ResizeKeyboard = true };
+
         await botClient.SendTextMessageAsync(
             text: greeting,
             chatId: message.Chat.Id,
+            replyMarkup: replyKeyboardMarkup,
             cancellationToken: cancellationToken);
     }
 
@@ -39,9 +45,9 @@ public partial class UpdateHandler
         };
 
         await botClient.SendTextMessageAsync(
-            message.Chat.Id, 
-            "Please select a setting:", 
-            replyMarkup: new ReplyKeyboardMarkup(keyboardLayout) { ResizeKeyboard = true }, 
+            message.Chat.Id,
+            "Please select a setting:",
+            replyMarkup: new ReplyKeyboardMarkup(keyboardLayout) { ResizeKeyboard = true },
             cancellationToken: cancellationToken);
-    } 
+    }
 }
