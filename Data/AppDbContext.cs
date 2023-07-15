ac5ea91fedbe865b88f350ec1b88cc8f946805e6
Mukhammadbilol <muhammadbilolumaroff@gmail.com>
2023-07-15 17:45:35 +0500

    Category entity added

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -4,18 +4,19 @@ using Microsoft.EntityFrameworkCore;
 public class AppDbContext : DbContext, IAppDbContext
 {
     public DbSet<User> Users { get; set; }
-    public AppDbContext(DbContextOptions<AppDbContext> options) 
+    public DbSet<Category> Categories { get; set; }
+    public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options) { }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<User>()
             .HasKey(e => e.Id);
-        
+
         modelBuilder.Entity<User>()
             .Property(e => e.Fullname)
             .HasMaxLength(100)
             .IsRequired();
-        
+
         modelBuilder.Entity<User>()
             .Property(e => e.Username)
             .HasMaxLength(50);
@@ -27,7 +28,7 @@ public class AppDbContext : DbContext, IAppDbContext
         modelBuilder.Entity<User>()
             .Property(e => e.Phone)
             .HasMaxLength(20);
-        
+
         modelBuilder.Entity<User>()
             .Property(e => e.CreatedAt)
             .HasDefaultValue(DateTime.UtcNow);
@@ -35,10 +36,26 @@ public class AppDbContext : DbContext, IAppDbContext
         modelBuilder.Entity<User>()
             .Property(e => e.ModifiedAt)
             .HasDefaultValue(DateTime.UtcNow);
-        
+
+        modelBuilder.Entity<Category>()
+            .HasKey(c => c.Id);
+
+        modelBuilder.Entity<Category>()
+            .Property(c => c.Name)
+            .IsRequired()
+            .HasMaxLength(125);
+
+        modelBuilder.Entity<Category>()
+            .HasData(
+                new { Id = 1, Name = "Food" },
+                new { Id = 2, Name = "Snack" },
+                new { Id = 3, Name = "Drink" },
+                new { Id = 4, Name = "Salad" },
+                new { Id = 5, Name = "Set" });
+
         base.OnModelCreating(modelBuilder);
     }
 
-    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default) 
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         => base.SaveChangesAsync(cancellationToken);
 }
---
+++ Data/Migrations/20230715123738_Category.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace durgerking.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Category : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(2023, 7, 15, 12, 37, 38, 142, DateTimeKind.Utc).AddTicks(9354),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTime(2023, 7, 10, 11, 29, 16, 314, DateTimeKind.Utc).AddTicks(6376));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(2023, 7, 15, 12, 37, 38, 142, DateTimeKind.Utc).AddTicks(7779),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTime(2023, 7, 10, 11, 29, 16, 314, DateTimeKind.Utc).AddTicks(6142));
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(125)", maxLength: 125, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Food" },
+                    { 2, "Snack" },
+                    { 3, "Drink" },
+                    { 4, "Salad" },
+                    { 5, "Set" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(2023, 7, 10, 11, 29, 16, 314, DateTimeKind.Utc).AddTicks(6376),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTime(2023, 7, 15, 12, 37, 38, 142, DateTimeKind.Utc).AddTicks(9354));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(2023, 7, 10, 11, 29, 16, 314, DateTimeKind.Utc).AddTicks(6142),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTime(2023, 7, 15, 12, 37, 38, 142, DateTimeKind.Utc).AddTicks(7779));
+        }
+    }
+}
