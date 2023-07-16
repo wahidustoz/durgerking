ea82e37baa28810cd0a7a70c8a35c8c6f18fc2b7
Wahid Abduhakimov <wakhid2802@gmail.com>
2023-07-17 00:20:57 +0900

    Remove update language line

--- Services/UpdateHandler.cs
+++ Services/UpdateHandler.cs
@@ -78,7 +78,6 @@ public partial class UpdateHandler : IUpdateHandler
          {
             user.Fullname = $"{telegramUser.FirstName} {telegramUser.LastName}";
             user.Username = telegramUser.Username;
-            user.Language = telegramUser.LanguageCode;
             user.ModifiedAt = DateTime.UtcNow;
             logger.LogInformation("user with ID {id} updated.", telegramUser.Id);
         }
