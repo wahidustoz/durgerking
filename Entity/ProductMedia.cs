b8d181487795c5b533669c535152be581d2f32a4
sarvar-swe <s.azodov.software.engineer@gmail.com>
2023-07-17 11:42:01 +0500

    deleted comments

--- Entity/ProductMedia.cs
+++ Entity/ProductMedia.cs
@@ -1,11 +1,13 @@
+namespace DurgerKing.Entity;
+
 public class ProductMedia
 {
     public Guid Id { get; set; }
-    public string MimeType { get; set; } // mp4 yoki shunga o'xshash file turi haqida malumot
-    public string Filename { get; set; } // user tomonidan berilgan fayl nomi
-    public string Extension{ get; set; } // user tomonidan berilgan fayl nomi
-    public int Order { get; set; }  // maxsulot rasmi nechinchi ochertda ko'rsatilishi 0-based index
-    public byte[] Data { get; set; } // byte array malumot. Max 5MB
+    public string MimeType { get; set; }
+    public string Filename { get; set; }
+    public string Extension{ get; set; }
+    public int Order { get; set; }
+    public byte[] Data { get; set; }
 
-    public virtual Product Product { get; set; } // qaysi maxsulot tegishli ekani haqida
+    public virtual Product Product { get; set; }
 }
