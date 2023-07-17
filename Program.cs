d8d64d873f77519a4d4ef96703b8b9269f052126
foziljonov.7 <89521523+foziljonov7@users.noreply.github.com>
2023-07-17 14:03:57 +0500

    Ignore null values in json response (#57)

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using DurgerKing.Entity.Data;
 using DurgerKing.Services;
 using Microsoft.EntityFrameworkCore;
@@ -6,7 +7,8 @@ using Telegram.Bot.Polling;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull);
 builder.Services.AddTransient<IUpdateHandler, UpdateHandler>();
 builder.Services.AddHostedService<BotStartingBackgroundService>();
 builder.Services.AddSingleton<ITelegramBotClient, TelegramBotClient>(
