4dd22ac3f85b856572d2f1823ea3732c9db4a347
Wahid Abduhakimov <wakhid2802@gmail.com>
2023-07-23 00:38:21 +0900

    Fixed valiidation and controller errors

--- Validators/CreateProductValidator.cs
+++ Validators/CreateProductValidator.cs
@@ -12,7 +12,7 @@ public class CreateProductValidator : AbstractValidator<CreateProductDto>
         RuleFor(dto => dto.Name).NotEmpty().MinimumLength(3).MaximumLength(50);
         RuleFor(dto => dto.Description).MaximumLength(500);
         RuleFor(dto => dto.Price).GreaterThan(0);
-        RuleFor(dto => dto.DiscountPercentage).InclusiveBetween(0, 100);
+        RuleFor(dto => dto.DiscountPercentage).InclusiveBetween(0, 1);
         RuleFor(dto => dto.CategoryId).GreaterThan(0);
 
         RuleFor(dto => dto.CategoryId).MustAsync(async (request, ctx, token)
