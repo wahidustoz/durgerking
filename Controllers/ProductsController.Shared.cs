bfbeb569ea5b14d8e48698164b60dadf3d111e5c
tparizoda <parizodatogaeva@gmail.com>
2023-07-22 01:09:11 -0700

    product controller break down

---
+++ Controllers/ProductsController.Shared.cs
@@ -0,0 +1,12 @@
+using DurgerKing.Entity.Data;
+
+namespace DurgerKing.Controllers;
+public partial class ProductsController
+{
+    private readonly IAppDbContext dbContext;
+
+    public ProductsController(IAppDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+}
