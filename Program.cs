f7c01904dd1d5b3a4772394ef0a4d1afa17cf822
Yasina2005 <126851231+Yasina2005@users.noreply.github.com>
2023-07-24 03:51:45 -0700

    add health middleware (#92)
    * add health middleware
    
    * namespace
    
    * using
    
    * add health middleware #92
    
    * add health middleware #92
    
    * Build fail fixed
    
    ---------
    
    Co-authored-by: Wahid Abduhakimov <wakhid2802@gmail.com>

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
+using DurgerKing;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -36,5 +37,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseHealth();
 app.MapControllers();
+
 app.Run();
