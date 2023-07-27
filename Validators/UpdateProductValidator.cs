937df764e55983c57354d1b499e13bc3a825e906
Wahid Abduhakimov <wakhid2802@gmail.com>
2023-07-28 03:37:22 +0900

    fixed minor standard issues (#112)

--- Validators/UpdateProductValidator.cs
+++ Validators/UpdateProductValidator.cs
@@ -10,13 +10,13 @@ public class UpdateProductValidator : AbstractValidator<UpdateProductDto>
     public UpdateProductValidator(IAppDbContext dbContext)
     {
         RuleFor(dto => dto.Name).NotEmpty().MinimumLength(3).MaximumLength(50);
-        RuleFor(dto => dto.Description).MaximumLength(500);
+        RuleFor(dto => dto.Description).NotEmpty().MaximumLength(500);
         RuleFor(dto => dto.Price).GreaterThan(0);
         RuleFor(dto => dto.DiscountPercentage).InclusiveBetween(0, 1);
         RuleFor(dto => dto.CategoryId).GreaterThan(0);
 
         RuleFor(dto => dto.CategoryId).MustAsync(async (request, ctx, token)
                 => await dbContext.Categories.AnyAsync(category => category.Id == request.CategoryId, token))
-            .WithMessage("Categegory with CategoryId doesn't exist in the system.");
+            .WithMessage("{PropertyName} with value {PropertyValue} does not exist.");
     }
 }
